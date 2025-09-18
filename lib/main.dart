996e8b35461e885384a4356882f5be46ae079ac1
anamartinez97m <anamartinez97m@gmail.com>
2025-09-18 10:13:58 +0200

    feat: added populated db

--- lib/main.dart
+++ lib/main.dart
@@ -2,40 +2,56 @@ import 'package:flutter/material.dart';
 
 import 'dart:async';
 // import 'package:flutter/widgets.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:io';
 
-void main() {
+Future<void> copyDatabaseFromAssets() async {
+  final dbPath = await sql.getDatabasesPath();
+  final pathToDb = path.join(dbPath, 'my_library.db');
+
+  //print('📂 Ruta donde debe estar la DB: $pathToDb');
+
+  final exists = await File(pathToDb).exists();
+  //print('exists: $exists');
+  if (!exists) {
+    final data = await rootBundle.load('assets/my_library.db');
+    final bytes = data.buffer.asUint8List(
+      data.offsetInBytes,
+      data.lengthInBytes,
+    );
+    await File(pathToDb).writeAsBytes(bytes, flush: true);
+  }
+}
+
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'my_library.db'),
+    version: 1,
+  );
+  return db;
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await copyDatabaseFromAssets();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Library',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'My Library'),
     );
   }
 }
@@ -59,18 +75,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _databaseTotalLength = 0;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    databaseData();
+  }
+
+  Future<void> databaseData() async {
+    final db = await _getDatabase();
+    final result = await db.rawQuery('select count(*) as count from book;');
+    print(result);
+    final count = result.first['count'] as int;
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _databaseTotalLength = count;
     });
   }
+
+  /*void addBook() {
+    setState(() {
+      
+    });
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -82,46 +110,27 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('The database contains the following number of books:'),
             Text(
-              '$_counter',
+              '$_databaseTotalLength',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+      /*floatingActionButton: FloatingActionButton(
+        onPressed: addBook,
+        elevation: 3,
+        tooltip: 'Add',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),*/
     );
   }
 }
