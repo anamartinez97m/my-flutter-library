37f9ff3efc9ac17a3380af53bbf2d9c3702fddfa
anamartinez97m <anamartinez97m@gmail.com>
2025-10-28 17:23:53 +0100

    changed main theme, and added floating btn in home to filter and order the booklist

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,17 @@ class MyApp extends StatelessWidget {
       title: 'My Library',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(fontSize: 28),
+          headlineMedium: TextStyle(fontSize: 24),
+          headlineSmall: TextStyle(fontSize: 20),
+          titleLarge: TextStyle(fontSize: 18),
+          titleMedium: TextStyle(fontSize: 16),
+          titleSmall: TextStyle(fontSize: 14),
+          bodyLarge: TextStyle(fontSize: 14),
+          bodyMedium: TextStyle(fontSize: 13),
+          bodySmall: TextStyle(fontSize: 12),
+        ),
       ),
       home: const NavigationScreen(),
     );
