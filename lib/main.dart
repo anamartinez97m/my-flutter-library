c35c645cc442da9f134d6d9ca038c88ce3a6dcfc
anamartinez97m <anamartinez97m@gmail.com>
2026-03-05 12:08:30 +0100

    feat: implemented firebase app check

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:myrandomlibrary/l10n/app_localizations.dart';
@@ -15,6 +17,10 @@ void main() async {
   // Initialize Firebase
   try {
     await Firebase.initializeApp();
+    await FirebaseAppCheck.instance.activate(
+      androidProvider:
+          kDebugMode ? AndroidProvider.debug : AndroidProvider.playIntegrity,
+    );
   } catch (e) {
     debugPrint('Error initializing Firebase: $e');
   }
